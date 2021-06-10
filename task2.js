01c9ce84b27dd6ab3b913a11e3da4b5b00db2b87
ruslanepi <ruslanep2@gmail.com>
2021-06-10 17:23:14 +0300

    add 2nd task

---
+++ task2.js
@@ -0,0 +1,34 @@
+// задача: распознать в строке правильную структура скобок {{}}
+// 
+
+
+const testString = "{{}} {}";
+
+
+
+function checkString(testString) {
+  let stack = [];
+  let flag;
+
+
+  for(let i = 0; i < testString.length; i++ ) {
+    
+    if(testString[i] === "{") {
+      stack.push(testString[i]);
+      
+    } else if(testString[i] !== " ") {
+      flag = stack.pop();
+      if( !flag  ) {
+        return false;
+      } 
+    }
+  }
+
+  // if(!stack.length) return false;
+  return true
+
+
+}
+
+
+console.log(checkString(testString)); 
